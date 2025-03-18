be287a5b22b1970fafd5ce5fa24b9ea1980f3d0b
Umesh Yadav <155528116+UmeshYadav21-Dev@users.noreply.github.com>
2025-03-18 13:04:25 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,10 +1,10 @@
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
 		int a = 10;
-=======
+
         String s = "hii";
->>>>>>> 71340b721f3457d615affd1ed0b5abfe98123b6b
+
         System.out.println("Hello, World!");
     }
 }
